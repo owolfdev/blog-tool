4a56a7d53cc2254cff0c5333d713f1e40b00abdb
owolfdev <owolfdev@gmail.com>
2023-04-22 05:27:49 +0700

    updates

--- components/BlogPosts.tsx
+++ components/BlogPosts.tsx
@@ -7,12 +7,12 @@ function BlogPosts({ posts }: any) {
   const dataContext = useContext(DataContext);
   return (
     <div>
-      <div className="mb-2 text-sm text-gray-500">{posts.length} posts</div>
+      <div className="mb-2 text-sm text-gray-500">{posts?.length} posts</div>
       <div className="space-y-5">
-        {posts.map((post: any) => {
+        {posts?.map((post: any) => {
           return (
             <div key={post.id} className="border rounded-xl">
-              <Link href={` /blog/${post.slug}?table=${dataContext.table}`}>
+              <Link href={`/blog/${post.slug}?table=${dataContext.table}`}>
                 <div className="p-3" key={post.id}>
                   <h3 className="text-2xl font-bold">{post.title}</h3>
                   <p>{post.excerpt}</p>
